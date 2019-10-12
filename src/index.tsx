7f9527a5546df193a6daa8fe42c6113e4fc4a2c7
mizchi <kotaro.chikuba@plaid.co.jp>
2019-10-13 07:15:50 +0900

    Download npm deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,20 @@
 import { h, render } from "preact";
+import Resolver from "./local-resolver/resolver";
+import NpmHttpRegistry from "./local-resolver/npm-http";
+
+function resolve(dependencies: any) {
+  const resolver = new Resolver({
+    registry: new NpmHttpRegistry({ registryUrl: "https://registry.npmjs.cf/" })
+  });
+  return resolver.resolve(dependencies);
+}
+
+resolve({
+  rxjs: "~5.5.0",
+  "left-pad": "*",
+  "zone.js": "latest",
+  "@angular/core": "~5.2.0"
+}).then(results => console.log(results));
+
 const root = document.querySelector(".root") as HTMLDivElement;
-render(<div>Hello</div>, root);
+render(<div>Hello2</div>, root);
